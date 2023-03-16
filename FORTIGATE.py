fd8bbea8ec30daa7d889880924c729111186b014
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-03-16 22:58:45 +0200

    raw log formatting [not tested yet]

--- FORTIGATE.py
+++ FORTIGATE.py
@@ -50,5 +50,29 @@ class Ticket:
         self.source_server = None           # is the src server?
 
 
+# format data of one ticket into parameters
+def format(values): 
+    # format the data in a dictionary
+    dictionary = {"NULL" : None}
+    # move character by character
+    start, end = 0, 1
+    inside_quotation = False
+    while end <= len(values):
+        # handle spaces inside quotation marks
+        # using a boolean value as a flag
+        if values == '\"': 
+            # flip flag
+            bracket_counter = not(bracket_counter)
+        # when parameter value ends
+        elif values[end] == ' ' and not(inside_quotation):
+            tmp = values[start:end].split('=')
+            dictionary[tmp[0]] = tmp[1]
+            start = end
+        # move to next character
+        end += 1
+    return dictionary
+
+
+
 if __name__ == "__main__": 
-    print(Warning.NoneMainModule)
+    print(notice.NoneMainModule)
