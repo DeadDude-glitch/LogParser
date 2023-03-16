f3abe95b5092be0f1966b4b7292ccdf10ecfa2bd
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-03-16 23:01:55 +0200

    planned module

---
+++ main.py
@@ -0,0 +1,6 @@
+# this module should act as an interface for user
+# read arguments when called and display help menu
+# heavily error handling should be applied here
+
+import Core
+ print("Hello, I am still under development.")
